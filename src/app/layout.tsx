bddc2effdc40cfc0dd3c5829f7b3d1d50ea4870e
sprintjy12 <sprint.jy12@gmail.com>
2026-07-23 17:31:31 +0900

    design: figma mcp 이용해 global font 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 
 import { Providers } from "@/app/providers";
 import "@/app/globals.css";
+
+const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  variable: "--font-pretendard",
+  weight: "45 920",
+});
 
 export const metadata: Metadata = {
   title: "간식대장",
@@ -14,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pretendard.variable}>
       <body>
         <Providers>{children}</Providers>
       </body>
